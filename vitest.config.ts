fab042f79170b07ace4462dcbf8b578d3c473c10
Mikael Lirbank <mikael@lirbank.com>
2025-06-11 10:53:03 -0700

    Explicitly load only needed env vars

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vitest/config";
 import dotenv from "dotenv";
+import fs from "node:fs";
 
-dotenv.config();
+const env = dotenv.parse(fs.readFileSync(".env", "utf8"));
 
-export default defineConfig({});
+export default defineConfig({
+  test: {
+    env: {
+      NEON_API_KEY: env.NEON_API_KEY,
+      NEON_PROJECT_ID: env.NEON_PROJECT_ID,
+    },
+  },
+});
