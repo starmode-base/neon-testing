8305f065d30843fa95ddf78ef400fa811dc8edec
Mikael Lirbank <mikael@lirbank.com>
2025-08-26 12:49:50 -0700

    Config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 dotenv.config();
 
@@ -16,14 +17,5 @@ export default defineConfig({
   test: {
     testTimeout: 10000,
   },
-  resolve: {
-    alias: {
-      // This alias allows us to import "neon-testing" in our test files, and
-      // have it resolve to the local "index.ts" file in this project. This
-      // ensures that tests always use the local source code, not a published
-      // package. This is only for this library, not for the project using it.
-      "neon-testing": path.resolve(__dirname, "index.ts"),
-      "neon-testing/utils": path.resolve(__dirname, "singleton.ts"),
-    },
-  },
+  plugins: [tsconfigPaths()],
 });
