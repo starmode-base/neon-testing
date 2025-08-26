0cae162326d5d1095cc31a3f820701673ddbce97
Mikael Lirbank <mikael@lirbank.com>
2025-08-26 12:53:33 -0700

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "vitest/config";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 dotenv.config();
@@ -10,8 +8,6 @@ dotenv.config();
 // in tests. The DATABASE_URL environment variable will be dynamically set by
 // `makeNeonTesting()()` in the test environment.
 delete process.env.DATABASE_URL;
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   test: {
