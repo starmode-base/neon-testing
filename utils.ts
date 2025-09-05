dbedf969c451adc0e9c51eaa11f685479471c450
Mikael Lirbank <mikael@lirbank.com>
2025-09-05 15:44:52 -0700

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -2,6 +2,7 @@ import { neonTesting as neonTestingVite } from "./vite-plugin.js";
 export { lazySingleton } from "./singleton.js";
 
 /**
- * @deprecated Import the Vite plugin from "neon-testing/vite" instead.
+ * @deprecated Import the Vite plugin from "neon-testing/vite" instead. This
+ * export will be removed in the next major version.
  */
 export const neonTesting = neonTestingVite;
