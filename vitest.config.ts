796c5cc9887a72eb2dacafe4cefd5411c50f4ccb
Mikael Lirbank <mikael@lirbank.com>
2026-06-02 11:56:07 -0700

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, configDefaults } from "vitest/config";
+import { defineConfig } from "vitest/config";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,8 +8,5 @@ export default defineConfig({
     setupFiles: ["neon-testing/setup"],
     testTimeout: 15_000,
     hookTimeout: 30_000,
-    exclude: [...configDefaults.exclude, "**/dist/**"],
-    // Limit to concurrent test files to avoid Neon API rate limiting
-    // maxConcurrency: 2,
   },
 });
